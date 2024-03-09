e6ab2c6adb63a659bdabb6c67e710e7ca06009be
Nicholas Cardoso <nicholas.cardoso@konia.com.br>
2024-03-09 15:07:54 -0300

    fix: layout not found

---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+import './globals.css'
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-br">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
