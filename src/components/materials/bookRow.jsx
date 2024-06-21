9a378802ca9f98f31f8cd9bafa70ed9105ce5108
TerryKrom <terrykrom1@gmail.com>
2024-06-21 08:36:38 -0300

    fix: signin responsivity fix and tested

--- src/components/materials/bookRow.jsx
+++ src/components/materials/bookRow.jsx
@@ -13,7 +13,6 @@ const BookRow = ({ slug }) => {
         setExpandedItem(null);
     }
 
-    // Concatenando os arrays diretamente
     const allBooks = [
         ...SocialScienceAndMath,
         ...Languages,
@@ -73,7 +72,7 @@ const BookRow = ({ slug }) => {
                             )}
                         </div>
                         <div className="flex items-center justify-between">
-                            <a href={book.src} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full">Ver Mais</a>
+                            <a target='_blank' href={book.src} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full">Ver Mais</a>
                         </div>
                     </div>
                 </div>
